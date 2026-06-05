1ea9a2326952114b4059ac5917378f931811f7b5
Simon <133875585+simpansoftware@users.noreply.github.com>
2026-06-05 20:01:09 +0200

    Update fastfetch.py

--- Windows/fastfetch.py
+++ Windows/fastfetch.py
@@ -1,7 +1,7 @@
 import zipfile
 import io
 
-r = requests.get("https://github.com/fastfetch-cli/fastfetch/releases/download/2.61.0/fastfetch-windows-amd64.zip")
+r = requests.get("https://simpansoftware.cc/rotarium-repo/Windows/fastfetch-windows-amd64.zip")
 
 with zipfile.ZipFile(io.BytesIO(r.content)) as z:
     z.extractall("packages/windows/fastfetch")
