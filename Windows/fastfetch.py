d6d3d1711e0d647e98ec16a3e32505d484644253
Simon <133875585+simpansoftware@users.noreply.github.com>
2026-06-05 16:59:47 +0200

    Update fastfetch.py

--- Windows/fastfetch.py
+++ Windows/fastfetch.py
@@ -4,4 +4,6 @@ import io
 r = requests.get("https://github.com/fastfetch-cli/fastfetch/releases/download/2.61.0/fastfetch-windows-amd64.zip")
 
 with zipfile.ZipFile(io.BytesIO(r.content)) as z:
-    z.extractall("packages/windows/fastfetch")
+    z.extractall("packages/linux/fastfetch")
+
+register_package("fastfetch", "packages/windows/fastfetch/fastfetch.exe")
