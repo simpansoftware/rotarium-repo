0ceae9307c4ec2aec538fb9a09a4cbd4e17f361a
Simon <133875585+simpansoftware@users.noreply.github.com>
2026-06-05 16:23:38 +0200

    Create fastfetch.py

---
+++ Windows/fastfetch.py
@@ -0,0 +1,7 @@
+import zipfile
+import io
+
+r = requests.get("https://github.com/fastfetch-cli/fastfetch/releases/download/2.61.0/fastfetch-windows-amd64.zip")
+
+with zipfile.ZipFile(io.BytesIO(r.content)) as z:
+    z.extractall("packages/windows/fastfetch")
