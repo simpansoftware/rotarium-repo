d8f36ba8f2de8047a87c45f48f416e5ae2dfc1e6
Simon <133875585+simpansoftware@users.noreply.github.com>
2026-06-05 17:35:26 +0200

    Update fastfetch.py

--- Linux/fastfetch.py
+++ Linux/fastfetch.py
@@ -1,9 +1,11 @@
 import zipfile
 import io
+import os
 
 r = requests.get("https://github.com/fastfetch-cli/fastfetch/releases/download/2.61.0/fastfetch-linux-amd64.zip")
 
 with zipfile.ZipFile(io.BytesIO(r.content)) as z:
     z.extractall("packages/linux/fastfetch")
 
+os.chmod("packages/linux/fastfetch/fastfetch-linux-amd64/usr/bin/fastfetch", 0o755)
 register_package("fastfetch", "packages/linux/fastfetch/fastfetch-linux-amd64/usr/bin/fastfetch", "packages/linux/fastfetch")
